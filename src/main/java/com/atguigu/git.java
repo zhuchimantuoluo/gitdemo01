76f9aa711dcc78eef5f4734436bc7539180bc5c9
another <another@163.com>
2021-08-16 21:48:42 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -7,5 +7,6 @@ public class git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix git4");
     }
 }
